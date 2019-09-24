6e28c140535c22405ee6343f4e734514d7865ea1
arcaster42 <tj.epperson070@gmail.com>
2019-09-24 12:01:07 +0900

    implemented icebreaks, cleaned up, debugged design

--- server.ts
+++ server.ts
@@ -121,15 +121,15 @@ class Client {
   }
 }
 
-class Player {
-  constructor(email, socket) {
-    this.email = email
-    this.socket = socket
-    this.heartbeat = true
-    this.answer = ''
-    this.hasAnswered = false
-  }
-}
+// class Player {
+//   constructor(email, socket) {
+//     this.email = email
+//     this.socket = socket
+//     this.heartbeat = true
+//     this.answer = ''
+//     this.hasAnswered = false
+//   }
+// }
 
 class Clients {
   constructor() {
@@ -144,46 +144,7 @@ class Clients {
   }
 }
 
-interface Games {
-  gameList: object
-}
-
-//Games and Games List => Client/Clientlist
-class Games {
-  constructor() {
-    this.gameList = {}
-    this.addGame = this.addGame.bind(this)
-  }
-  addGame(quizGame: QuizGame) {
-    this.gameList[quizGame.id] = quizGame
-  }
-}
-
-//id should match hangout id
-//validPlayers = user emails
-interface QuizGame {
-  id: string
-  playerList: object
-  validPlayers: Array<string>
-  answers: number
-}
-
-class QuizGame {
-  constructor(id, validPlayers) {
-    this.id = id
-    this.playerList = {}
-    this.validPlayers = validPlayers
-    this.answers = 0
-    this.addPlayer = this.addPlayer.bind(this)
-  }
-  addPlayer(client: Client) {
-    let player = new Player(client.email, client.socket)
-    this.playerList[player.email] = player
-  }
-}
-
 const clients = new Clients()
-const games = new Games()
 
 const httpServer = createServer(app)
 const wss = new SocketServer({ server: httpServer })
@@ -196,7 +157,6 @@ async function getToken(email) {
           }
         }
       `
-      console.log("line 204")
       let tokenResult = await axios.post('https://asobu-staging.herokuapp.com/graphql', { 
         query: print(query), 
         variables: {
@@ -246,6 +206,7 @@ wss.on('connection', ws => {
       let newClient = new Client(message[1], ws, message[2])
       clients.saveClient(newClient)
       console.log(`${message[1]} has logged in. Token is ${message[2]}.`)
+      clients.clientList[message[1]].isAlive = true
       const pulseCheck = setInterval(() => {
         if (!newClient.heartbeat) {
           clients.removeClient(newClient.email)
@@ -340,29 +301,29 @@ wss.on('connection', ws => {
         console.log(`${message[2]} was notified.`)
     }
 
-    //send game question as well. will have to split by commas instead of spaces
-    //will alert user with textinput
-    //send answers to each user
-    //q0 starts game, q1 sends question, q2 sends answer
-    //[0] - Quiz Game Code, [1] - Origin Email, [2] - Origin First Name, [3] - Hangout ID, [4] - Partner Email
+    //[0] - Quiz Game Code, [1] - Origin Email, [2] - Origin First Name, [3] - Partner Email
     if (message[0] === 'q0') {
-      if (games.gameList[message[2]] && clients.clientList[message[1]]) {
-        //do we need to create a new Player instance?
-        games.gameList[message[2]].addPlayer(clients.clientList[message[1]])
-      } else {
-        const newQuizGame = new QuizGame(message[2], [message[1], message[3]])
-        newQuizGame.addPlayer(clients.clientList[message[1]])
-        games.addGame(newQuizGame)
+      console.log(`${message[1]} has requested to start a game with ${message[3]}`)
+      if (clients.clientList[message[3]]) {
+        clients.clientList[message[3]].socket.send(`q0 ${message[1]} ${message[2]}`)
+        console.log(`${message[3]} was notified.`)
       }
     }
-    //Can we export array of questions and simply send an index over the websocket?
-    //[0] - Quiz Game Code, [1] - Origin Email, [2] Partner Email, [3] Question #?
+    //[0] - Quiz Accepted Code, [1] - Origin Email, [2] Partner Email
     if (message[0] === 'q1') {
-    
-    }
-    //[0] - Quiz Game Code, [1] - Origin Email, [2] Partner Email, [3] Next Question #, [4] Input
-    if (message[0] === 'q2') {
-
+      const randomNumber = Math.floor(Math.random() * 12)
+      if (clients.clientList[message[1]] && clients.clientList[message[2]]) {
+        clients.clientList[message[1]].socket.send(`q1 ${randomNumber} ${message[2]}`)
+        console.log(`${message[1]} was sent a question.`)
+        clients.clientList[message[2]].socket.send(`q1 ${randomNumber} ${message[1]}`) 
+        console.log(`${message[2]} was sent a question.`)
+      } else {
+        //Send error message if partner dropped
+        if (clients.clientList[message[1]]) {
+          clients.clientList[message[1]].socket.send('q9')
+          console.log(`${message[1]} was sent an error code.`)
+        }
+      }
     }
   })
 
